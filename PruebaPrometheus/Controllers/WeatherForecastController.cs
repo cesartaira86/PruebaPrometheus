c7df81e0ea4a9d55e220bb02eba82e161ddaf11f
Cesar <cesartaira86@gmail.com>
2021-12-19 21:34:53 -0500

    Añadiendo estadíticas de prometheus

--- PruebaPrometheus/Controllers/WeatherForecastController.cs
+++ PruebaPrometheus/Controllers/WeatherForecastController.cs
@@ -3,7 +3,9 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Prometheus;
 
 namespace PruebaPrometheus.Controllers
 {
@@ -11,11 +13,15 @@ namespace PruebaPrometheus.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        private static readonly Histogram LoginDuration = Metrics
+            .CreateHistogram("myapp_login_duration_seconds", "Histogram of login call processing durations.");
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
-
+        private static readonly Counter ProcessedJobCount = Metrics
+            .CreateCounter("myapp_jobs_processed_total", "Number of processed jobs.");
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -26,14 +32,24 @@ namespace PruebaPrometheus.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            ProcessedJobCount.Inc();
+            using (LoginDuration.NewTimer())
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                var rng = new Random();
+                
+                Random random = new Random();
+                int num = random.Next(1000);  
+                
+                Thread.Sleep(num);
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                    {
+                        Date = DateTime.Now.AddDays(index),
+                        TemperatureC = rng.Next(-20, 55),
+                        Summary = Summaries[rng.Next(Summaries.Length)]
+                    })
+                    .ToArray();
+
+            }
         }
     }
 }
